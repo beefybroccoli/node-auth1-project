72c0022ced5b68828f8a462665e16a7fd70ff95c
beefybroccoli <tedkimemail@yahoo.com>
2021-12-13 17:35:21 -0800

    ...

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,21 +4,21 @@ const db = require("../../data/db-config");
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 async function find() {
-  return await db.get('users');
+  return await db('users');
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 async function findBy(filter) {
-  return await db.get('users').where(filter);
+  return await db('users').where(filter);
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 async function findById(user_id) {
-  const result = await db.get('users').where('user_id', user_id);
+  const result = await db('users').where('user_id', user_id);
   return result[0];
 }
 
@@ -26,8 +26,8 @@ async function findById(user_id) {
   resolves to the newly inserted user { user_id, username }
  */
 async function add(user) {
-  const newUserId = await db.get('users').insert(user);
-  const newUser = await db.get('users').where("users_id", newUserId[0]);
+  const newUserId = await db('users').insert(user);
+  const newUser = await db('users').where("users_id", newUserId[0]);
   return newUser;
 }
 
